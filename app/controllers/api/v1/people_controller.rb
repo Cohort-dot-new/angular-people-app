693d9cfec6bb1da44f8836ed56caa25f4f983a4b
Peter Jang <peter.x.jang@gmail.com>
2016-12-22 20:05:42 -0600

    Add create API action and create angular action

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -3,4 +3,13 @@ class Api::V1::PeopleController < ApplicationController
     @people = Person.all
     render 'index.json.jbuilder'
   end
+
+  def create
+    @person = Person.new(
+      name: params[:name],
+      bio: params[:bio]
+    )
+    @person.save
+    render 'show.json.jbuilder'
+  end
 end
